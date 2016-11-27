a158579ba0e115e3d971d6b170e1a864305137e9
Tim Trittipo <Tim.Trittipo@am.sony.com>
2016-11-26 20:00:34 -0800

    export vars to file

--- export-image/prerun.sh
+++ export-image/prerun.sh
@@ -31,12 +31,13 @@ SECTOR_SIZE=512
 SZ=$SECTOR_SIZE
 # calculate sectors in each partion
 P1_SECTORS=$(expr $P1_MB \* 1024 \* 1024 \/ $SZ )
+P1_SECTORS=$(expr $P1_SECTORS \+ $P1_OFFSET )
+
 P2_SECTORS=$(expr $P2_MB \* 1024 \* 1024 \/ $SZ )
 P3_SECTORS=$(expr $P3_MB \* 1024 \* 1024 \/ $SZ )
 P4_SECTORS=$(expr $P4_MB \* 1024 \* 1024 \/ $SZ )
 
 #add the inital offset to factor
-P1_SECTORS=$(expr $P1_SECTORS \+ $P1_OFFSET )
 
 P1_START_SEC=$P1_OFFSET
 P2_START_SEC=$(expr $P1_SECTORS + 1 )
@@ -51,6 +52,17 @@ echo $P3_START_SEC     $P2_SECTORS
 echo $P4_START_SEC     $P3_SECTORS
 
 
+echo
+"
+P1_SECTORS=$P1_SECTORS
+P2_SECTORS=$P2_SECTORS
+P3_SECTORS=$P3_SECTORS
+P4_SECTORS=$P4_SECTORS
+P1_START_SEC=$P1_OFFSET
+P2_START_SEC=$(expr $P1_SECTORS + 1 )
+P3_START_SEC=$(expr $P2_START_SEC \+ $P2_SECTORS + 1 )
+P4_START_SEC=$(expr $P3_START_SEC \+ $P3_SECTORS + 1 )
+" >> ${EXPORT_ROOTFS_DIR}/_fs_info
 
 fallocate -l ${IMG_SIZE} ${IMG_FILE}
 # t type
@@ -95,30 +107,11 @@ _mkparts
 LOOP_DEV=`kpartx -asv ${IMG_FILE} | grep -E -o -m1 'loop[[:digit:]]+' | head -n 1`
 BOOT_DEV=/dev/mapper/${LOOP_DEV}p1
 ROOT_DEV=/dev/mapper/${LOOP_DEV}p2
-ROOT2_DEV=/dev/mapper/${LOOP_DEV}p3
-OPT_DEV=/dev/mapper/${LOOP_DEV}p4
+# ROOT2_DEV=/dev/mapper/${LOOP_DEV}p3
+# OPT_DEV=/dev/mapper/${LOOP_DEV}p4
 
 mkdosfs -n boot -S 512 -s 16 -v $BOOT_DEV > /dev/null
 mkfs.ext4 -O ^huge_file $ROOT_DEV > /dev/null
-
-# create after to reduce image write times
-# #savetime
-# mkfs.ext4 -O ^huge_file $ROOT2_DEV > /dev/null
-
-
-_lvm_setup(){
- echo "doing lvm setup"
-  # create a
-  # pv
-  # vg
-  # lv
-  # ext4 fs
-
-  # # mkfs.ext4 -O ^huge_file $OPT_DEV > /dev/null
-
-}
-_lvm_setup
-
 
 mount -v $ROOT_DEV ${ROOTFS_DIR} -t ext4
 mkdir -p ${ROOTFS_DIR}/boot
