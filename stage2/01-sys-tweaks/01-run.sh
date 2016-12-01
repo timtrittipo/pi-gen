d75ab17e30b5fc90d0b2a48a599aae1be1ae33aa
Tim <tim.trittipo@am.sony.com>
2016-11-30 16:40:02 -0800

    allow node url to be env option, rm ln to node, add path

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -1,16 +1,12 @@
 #!/bin/bash -e
 
-NODE_URL="https://nodejs.org/dist/v6.9.1/node-v6.9.1-linux-armv7l.tar.xz"
-
-install -m 755 files/regenerate_ssh_host_keys		${ROOTFS_DIR}/etc/init.d/
+install -m 755 files/regenerate_ssh_host_keys ${ROOTFS_DIR}/etc/init.d/
 install -m 755 files/apply_noobs_os_config		${ROOTFS_DIR}/etc/init.d/
-install -m 755 files/resize2fs_once			${ROOTFS_DIR}/etc/init.d/
-
-install -d						${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d
-install -m 644 files/ttyoutput.conf			${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d/
-
+install -m 755 files/resize2fs_once			      ${ROOTFS_DIR}/etc/init.d/
+install -d						              ${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d
+install -m 644 files/ttyoutput.conf	${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d/
 install -m 644 files/50raspi				${ROOTFS_DIR}/etc/apt/apt.conf.d/
-
+install -m 755 files/nodepath.sh    ${ROOTFS_DIR}/etc/profile.d/
 
 on_chroot sh -e - <<EOF
 systemctl disable hwclock.sh
@@ -40,23 +36,16 @@ usermod --pass='*' root
 EOF
 
 _install_node(){
-
+	${NODE_URL:="https://nodejs.org/dist/v6.9.1/node-v6.9.1-linux-armv7l.tar.xz"}
 	BIN_DIR=${SCRIPT_DIR}/binaries
-
 	NODE_TAR=${BIN_DIR}/node.tar.xz
-	mkdir -p ${BIN_DIR}
 	NODE_ROOTFS_DEST=/usr/local/node
 	NODE_DEST=${ROOTFS_DIR}/${NODE_ROOTFS_DEST}
+
+	mkdir -p ${BIN_DIR}
 	install -d ${NODE_DEST}
-
-	wget -nc "$NODE_URL" -O $NODE_TAR || true
+	wget -c -q -N "$NODE_URL" -O $NODE_TAR || true
 	tar xf $NODE_TAR  -C ${NODE_DEST} --strip=1
-
-on_chroot sh -e - <<EOF
-  cd /usr/bin && ln -sv ${NODE_ROOTFS_DEST}/bin/node . && ln -sv ${NODE_ROOTFS_DEST}/bin/npm .
-EOF
-# ${NODE_ROOTFS_DEST}/bin/npm -g pm2
-
 }
 
 _install_node
